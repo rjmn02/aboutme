a496d82a1a7d8e07beae7a2daac74195a4d984b0
Rhyss <rhyssjmn@gmail.com>
2023-10-13 22:05:53 +0800

    larot

---
+++ code.js
@@ -0,0 +1,11 @@
+function clicked(){
+    var par = document.createElement(p);
+    par.textContent = "clicked";
+    document.body.appendChild(par);
+}
+
+function back(){
+    console.log("tanginamo");
+}
+
+back();

c32f4e6aa73338651b0e6736fc81566cef81bd0a
Rhyss <rhyssjmn@gmail.com>
2023-11-09 17:38:26 +0800

    added functionality

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
-function bgAnimation(){
-    
+function main(){
+    facebookButton()
+    igButton()
+    githubButton()
+    gmailButton()
 }
+
+function bgAnimation(){
+
+}
+
+function facebookButton(){
+    let button = document.getElementById("fbButton");
+    button.addEventListener("click", () => {
+        window.open("https://www.facebook.com/christianrhyss.jimenez123");
+    });
+}
+
+function igButton(){
+    let button = document.getElementById("igButton");
+    button.addEventListener("click", () => {
+        window.open("https://www.instagram.com/rjmn0208/");
+    });
+}
+
+function githubButton(){
+    let button = document.getElementById("githubButton");
+    button.addEventListener("click", () => {
+        window.open("https://github.com/rjmn02");
+    });
+}
+
+function gmailButton(){
+    let button = document.getElementById("gmailButton");
+    button.addEventListener("click", () => {
+        window.open("https://github.com/rjmn02");
+    });
+}
+main()
